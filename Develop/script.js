0a731ad3b069e516963c8e2da2f880fe104e4740
Josh <joshuaelitate@gmail.com>
2023-04-06 01:12:06 -0700

    finished functionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,61 +1,75 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+var button = document.getElementsByClassName("saveBtn");
 var today = dayjs();
-$('#currentDay').text(today.format('MMM D, YYYY'));
+var currentHour = today.format('HH')
+$(function () {
+toString(currentHour)
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+  $('#currentDay').text(today.format('MMM D, YYYY'));
 
-// grabs the input variables
-var formEl = $('#skills-form');
-var nameInputEl = $('#skill-name');
-var dateInputEl = $('#datepicker');
-var skillsListEl = $('#skills-list');
+  $('.row').each(function() {
+    // Get the hour from the ID of the time block
+    var hour = parseInt($(this).attr('id'));
 
-// handles the input and prints them
-var handleFormSubmit = function (event) {
-  event.preventDefault();
+    // Compare the hour to the current hour and apply the appropriate class
+    if (hour < currentHour) {
+      $(this).addClass('past');
+      $(this).removeClass('present future')
+      return
+    } else if (hour === currentHour) {
+      $(this).addClass('present');
+      $(this).removeClass('past future')
+      return
+    } else if (hour > currentHour) {
+      $(this).addClass('future');}
+      $(this).removeClass('present past')
+  });
 
-  var nameInput = nameInputEl.val();
-  var dateInput = dateInputEl.val();
-
-  if (!nameInput || !dateInput) {
-    console.log('You need to fill out the form!');
-    return;
+  function saveComment(name, myParent) {
+    var commentHour = parseInt($(this).attr('id'))
+    var commentId = commentHour + 1
+    var note = $(`#${commentId}`);
+    console.log(note.val())
+    var savedNote = {
+      note: note.val()
+    };
+    localStorage.setItem(`savedNote${commentHour}`, JSON.stringify(savedNote));
   }
 
-  printSkills(nameInput, dateInput);
+  for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', saveComment, false);
+  }
 
-  // resets form
-  nameInputEl.val('');
-  dateInputEl.val('');
-};
-// grabs the variable and creates a submit
-formEl.on('submit', handleFormSubmit);
+    function render() {
+        var renderNote0 = JSON.parse(window.localStorage.getItem`savedNote20`)
+        var renderNote1 = JSON.parse(window.localStorage.getItem`savedNote22`)
+        var renderNote2 = JSON.parse(window.localStorage.getItem`savedNote24`)
+        var renderNote3 = JSON.parse(window.localStorage.getItem`savedNote26`)
+        var renderNote4 = JSON.parse(window.localStorage.getItem`savedNote28`)
+        var renderNote5 = JSON.parse(window.localStorage.getItem`savedNote30`)
+        var renderNote6 = JSON.parse(window.localStorage.getItem`savedNote32`)
+        var renderNote7 = JSON.parse(window.localStorage.getItem`savedNote34`)
+        var renderNote8 = JSON.parse(window.localStorage.getItem`savedNote36`)
+        
+        ifTrue(renderNote0, 21)
+        ifTrue(renderNote1, 23)
+        ifTrue(renderNote2, 25)
+        ifTrue(renderNote3, 27)
+        ifTrue(renderNote4, 29)
+        ifTrue(renderNote5, 31)
+        ifTrue(renderNote6, 33)
+        ifTrue(renderNote7, 35)
+        ifTrue(renderNote8, 37)
+    }
 
-// puts the variables into the other form
-var printSkills = function (name, date) {
-  var listEl = $('<li>');
-  var listDetail = name.concat(' on ', date);
-  listEl.addClass('list-group-item').text(listDetail);
-  listEl.appendTo(skillsListEl);
-};
+    function ifTrue (noteRender, id) {
+      if(noteRender !== null){
+        document.getElementById(id).innerHTML = noteRender.note
+      } else {
+        return
+      }
+    }
+    render()
+  })
